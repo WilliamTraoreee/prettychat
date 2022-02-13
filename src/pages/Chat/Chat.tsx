34a2caf3eb5c5483c8a8fc72bab88f0ccfb63d74
BeardedBear <germain.poirrier@gmail.com>
2022-02-13 23:09:21 +0100

    Re-move tmi events from useeffect

--- src/pages/Chat/Chat.tsx
+++ src/pages/Chat/Chat.tsx
@@ -14,69 +14,70 @@ export default function Chat() {
   const themeChoose: string | null = searchParams.get("theme");
   const client = new tmi.Client({ channels: [username || ""] });
 
+  client.on("message", (_, tags, message) => {
+    const options: EmoteOptions = {
+      format: "default",
+      themeMode: "light",
+      scale: "2.0",
+    };
+
+    const msg: Message = {
+      id: tags?.id,
+      username: tags["display-name"],
+      twitch: tags?.username,
+      emotes: tags?.emotes || {},
+      date: new Date(),
+      message,
+      badges: tags?.badges,
+      mod: tags?.mod,
+      subscriber: tags?.subscriber,
+      color: tags?.color,
+    };
+
+    msg.message = parse(msg.message, msg.emotes, options);
+
+    setMessages((oldMessages) => {
+      if (oldMessages.length >= 50) oldMessages.shift();
+      return [...oldMessages, msg];
+    });
+  });
+
+  client.on("connected", () => {
+    console.log("Je suis bien connecté");
+  });
+
+  client.on("messagedeleted", (_channel, _username, _deleteMessage, userstate) => {
+    setMessages((msgs: Message[]) => {
+      const msgId = userstate["target-msg-id"];
+      const allMsgs = [...msgs];
+      const cleanMsgs = allMsgs.filter((m) => m.id !== msgId);
+
+      return cleanMsgs;
+    });
+  });
+
+  client.on("ban", (_channel, username) => {
+    setMessages((msgs) => {
+      const allMsgs = [...msgs];
+      const cleanMsgs = allMsgs.filter((m) => m.twitch !== username);
+
+      return cleanMsgs;
+    });
+  });
+
+  client.on("timeout", (_channel, username) => {
+    setMessages((msgs) => {
+      const allMsgs = [...msgs];
+      const cleanMsgs = allMsgs.filter((m) => m.twitch !== username);
+
+      return cleanMsgs;
+    });
+  });
+
+  client.on("clearchat", () => setMessages([]));
+
   useEffect(() => {
     client.connect();
-    client.on("message", (_, tags, message) => {
-      const options: EmoteOptions = {
-        format: "default",
-        themeMode: "light",
-        scale: "2.0",
-      };
-
-      const msg: Message = {
-        id: tags?.id,
-        username: tags["display-name"],
-        twitch: tags?.username,
-        emotes: tags?.emotes || {},
-        date: new Date(),
-        message,
-        badges: tags?.badges,
-        mod: tags?.mod,
-        subscriber: tags?.subscriber,
-        color: tags?.color,
-      };
-
-      msg.message = parse(msg.message, msg.emotes, options);
-
-      setMessages((oldMessages) => {
-        if (oldMessages.length >= 50) oldMessages.shift();
-        return [...oldMessages, msg];
-      });
-    });
-
-    client.on("connected", () => {
-      console.log("Je suis bien connecté");
-    });
-
-    client.on("messagedeleted", (_channel, _username, _deleteMessage, userstate) => {
-      setMessages((msgs: Message[]) => {
-        const msgId = userstate["target-msg-id"];
-        const allMsgs = [...msgs];
-        const cleanMsgs = allMsgs.filter((m) => m.id !== msgId);
-
-        return cleanMsgs;
-      });
-    });
-
-    client.on("ban", (_channel, username) => {
-      setMessages((msgs) => {
-        const allMsgs = [...msgs];
-        const cleanMsgs = allMsgs.filter((m) => m.twitch !== username);
-
-        return cleanMsgs;
-      });
-    });
-
-    client.on("timeout", (_channel, username) => {
-      setMessages((msgs) => {
-        const allMsgs = [...msgs];
-        const cleanMsgs = allMsgs.filter((m) => m.twitch !== username);
-
-        return cleanMsgs;
-      });
-    });
-
-    client.on("clearchat", () => setMessages([]));
   }, []);
 
   switch (themeChoose) {
